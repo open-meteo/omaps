bee28ef7058c995d9be3136d898214513771e305
Vincent van der Wal <info@vincentvanderwal.nl>
2025-06-10 10:50:46 +0200

    add test for RotatedLatLon

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { pad } from '../utils/pad';
-import { LambertConformalConicProjection } from '../utils/projection';
+import { RotatedLatLonProjection, LambertConformalConicProjection } from '../utils/projection';
 
 // --- pad ---
 test('check pad with 1 digit', () => {
@@ -20,6 +20,18 @@ test('Test LambertConformalConicProjection for DMI', () => {
 	expect(proj.n).toBe(0.8241261886220157);
 	expect(proj.λ0).toBe(-0.13962634015954636);
 	expect(proj.R).toBe(6371229);
+
 	expect(proj.forward(39.671, -25.421997)[0]).toBe(-1527524.6244234492);
 	expect(proj.forward(39.671, -25.421997)[1]).toBe(-1588681.0428292789);
+});
+
+test('Test RotatedLatLon for KNMI', () => {
+	const proj = new RotatedLatLonProjection({
+		rotation: [-35, -8]
+	});
+	expect(proj.θ).toBe(0.9599310885968813);
+	expect(proj.ϕ).toBe(-0.13962634015954636);
+
+	expect(proj.forward(39.671, -25.421997)[0]).toBe(-13.716985366241445);
+	expect(proj.forward(39.671, -25.421997)[1]).toBe(-13.617348599940314);
 });
